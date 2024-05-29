207696eb3e0450f8f680c19e4c1ae8138a9a2694
Davi Hasson Castro <davihasson@gmail.com>
2024-05-29 16:53:54 -0300

    Revert "feat: new admin page"
    This reverts commit 0915c8a9d52c15fb0e48b64765526ecb090afca5.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,26 +1,3 @@
-import Link from "next/link";
-import { Button } from "../_components/ui/button";
-
 export default function Page() {
-  return (
-    <div className="px-4 py-12 flex flex-col gap-12 max-w-screen-xl mx-auto">
-      <div className="flex flex-col">
-        <h1 className="font-extrabold text-gradient text-4xl sm:5xl md:text-6xl">
-          Dashboard de Administrador
-        </h1>
-        <p className="text-muted-foreground text-sm">
-          Aqui você pode <strong className="text-white">administrar</strong>{" "}
-          todos os usuários e os links de configuração de broadcast.
-        </p>
-      </div>
-      <div className="flex items-center gap-3">
-        <Button variant="outline" asChild>
-          <Link href="/admin/users">Usuários</Link>
-        </Button>
-        <Button asChild>
-          <Link href="/admin/intranet">Intranets</Link>
-        </Button>
-      </div>
-    </div>
-  );
+  return <></>;
 }
